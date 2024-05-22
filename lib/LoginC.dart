b04e73975286b47256bda476676e51a471e8d28c
Nazim Raian Khan <nazimraiank@gmail.com>
2024-05-22 22:01:37 +0600

    Transition Added

--- lib/LoginC.dart
+++ lib/LoginC.dart
@@ -1,8 +1,13 @@
 import 'package:assignment1/Reg.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginC extends GetxController{
   loginMessageShow() => print('Pressed Login');
   regMessageShow() => print('Pressed Register');
-  getToRegPage() => Get.to(Reg());
+  getToRegPage() => Get.to(
+      const Reg(),
+      transition: Transition.downToUp,
+      duration: Durations.long1,
+  );
 }
--- lib/RegC.dart
+++ lib/RegC.dart
@@ -1,8 +1,13 @@
 import 'package:assignment1/Login.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegC extends GetxController{
   regMessageShow() => print('Pressed sign up');
-  getToLoginPage() => Get.offAll(Login());
+  getToLoginPage() => Get.offAll(
+    const Login(),
+    transition: Transition.rightToLeft,
+    duration: Durations.extralong1,
+  );
 }
 
